f9c4499a33b058e0ad20a9c519d25813065e42f7
JamesWilliamson999 <157648828+JamesWilliamson999@users.noreply.github.com>
2024-11-30 00:53:38 -0800

    Update Testbench.sv

--- Testbench.sv
+++ Testbench.sv
@@ -1,3 +1,4 @@
+module testbench();
   logic [15:0] instruction;
   logic signed [15:0] register [15:0];
  
@@ -23,6 +24,22 @@
    instruction = 16'b0010_0000_0001_0011;
 	#1;
    $display("%0d * %0d = %0d",register[instruction[11:8]],register[instruction[7:4]],register[instruction[3:0]]);
+       //tests AND
+   instruction = 16'b0011_0000_0001_0011;
+	#1;
+   $display("%0b & %0b = %0b",register[instruction[11:8]],register[instruction[7:4]],register[instruction[3:0]]);
+     //tests Or
+   instruction = 16'b0100_0000_0001_0011;
+	#1;
+   $display("%0b | %0b = %0b",register[instruction[11:8]],register[instruction[7:4]],register[instruction[3:0]]);
+   //tests Xor
+   instruction = 16'b0101_0000_0001_0011;
+	#1;
+   $display("%0b ^ %0b = %0b",register[instruction[11:8]],register[instruction[7:4]],register[instruction[3:0]]);
+   //tests Not
+   instruction = 16'b0110_0000_0001_0011;
+	#1;
+   $display("~%0b = %0b",register[instruction[11:8]],register[instruction[3:0]]);
    
    $finish;
  end  
